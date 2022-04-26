8cbdbabe88227a8672c034bca664b8e7b2bba157
Sagi Shadur <saroad2@gmail.com>
2022-04-26 10:24:24 +0300

    Fix bug in io_util.py

--- src/ogle/io_util.py
+++ src/ogle/io_util.py
@@ -4,9 +4,9 @@ from ogle.random_data import generate_parabolic_data
 
 
 def read_data(data_path=None, is_random=False):
-    if data_path is not None or not is_random:
-        if not is_random:
-            data_path = DEFAULT_DATA_PATH
-        df = pd.read_csv(data_path)
-        return None, df["x"].to_numpy(), df["y"].to_numpy()
-    return generate_parabolic_data()
+    if is_random:
+        return generate_parabolic_data()
+    if data_path is None:
+        data_path = DEFAULT_DATA_PATH
+    df = pd.read_csv(data_path)
+    return None, df["x"].to_numpy(), df["y"].to_numpy()
