05b8901e8c7aeb1296cbd3b12418e2c2ed3d0615
Sagi Shadur <saroad2@gmail.com>
2022-05-10 13:08:03 +0300

    Implement 4d monte carlo

--- src/ogle/cli/grid_search_cli.py
+++ src/ogle/cli/grid_search_cli.py
@@ -10,6 +10,7 @@ from ogle.constants import (
     DEFAULT_EXPERIMENTS,
     DEFAULT_SPACE_SEARCH,
     GRID_SEARCH_2D_NAMES,
+    GRID_SEARCH_4D_NAMES,
 )
 from ogle.grid_search import iterative_grid_search
 from ogle.io_util import read_data
@@ -153,3 +154,53 @@ def grid_search_4d_cli(data_path, search_space, chi2_epsilon):
             output_dir=output_dir,
             index=i,
         )
+
+
+@grid_search_cli_group.command("4d-monte-carlo")
+@click.argument("data_path", type=click.Path(exists=True, path_type=Path))
+@click.option("-s", "--search-space", type=int, default=DEFAULT_SPACE_SEARCH)
+@click.option("-e", "--experiments", type=int, default=DEFAULT_EXPERIMENTS)
+@click.option("--normal-curve/--no-normal-curve", is_flag=True, default=True)
+@click.option("-c", "--chi2-epsilon", type=float, default=CHI2_EPSILON)
+@click.option("-w", "--workers", type=int, default=1)
+def monte_carlo_4d_cli(
+    data_path, search_space, experiments, chi2_epsilon, normal_curve, workers
+):
+    if data_path.is_dir():
+        data_path = data_path / f"{DATA_FILE_NAME}.csv"
+    data_dir = data_path.parent
+    _, x, y, yerr = read_data(data_path=data_path, is_random=False)
+    with (data_dir / "parabolic_fitting_results" / "fit_Result.json").open(
+        mode="r"
+    ) as fd:
+        results_json = json.load(fd)
+    t0_candidate, t0_step = results_json["t0"], results_json["t0_error"]
+    u_min_candidate, u_min_step = results_json["u_min"], results_json["u_min_error"]
+    tau_candidate, tau_step = results_json["tau"], results_json["tau_error"]
+    fbl_candidate, fbl_step = 1, 2 / search_space
+    results = Parallel(n_jobs=workers, verbose=5)(
+        delayed(iterative_grid_search)(
+            x=x,
+            y=y,
+            yerr=yerr,
+            constants_dict={},
+            candidates_dict=dict(
+                t0=t0_candidate,
+                tau=tau_candidate,
+                u_min=u_min_candidate,
+                f_bl=fbl_candidate,
+            ),
+            steps_dict=dict(t0=t0_step, tau=tau_step, u_min=u_min_step, f_bl=fbl_step),
+            search_space=search_space,
+            chi2_epsilon=chi2_epsilon,
+            sample=True,
+            history=False,
+        )
+        for _ in range(experiments)
+    )
+    plot_monte_carlo_results(
+        results,
+        parameters=GRID_SEARCH_4D_NAMES + ["iterations"],
+        output_dir=data_dir / "grid_search_4d_monte_carlo_results",
+        normal_curve=normal_curve,
+    )
